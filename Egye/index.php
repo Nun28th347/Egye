fd4ec8c276e09722591463adcabe81b3077b5372
Nun28th347 <61735218+Nun28th347@users.noreply.github.com>
2023-10-11 20:05:51 +0700

    first

---
+++ Egye/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sahara</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@1,200&family=Roboto+Serif:ital,wght@1,200&display=swap" rel="stylesheet">
+
+    <link rel="icon" href="imgs/sand-logos.jpeg">
+
+</head>
+<body>
+    
+    <div class="container">
+        <div class="navbar">
+            <img src="imgs/logos_white.png" class="logo">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">HOME</a>
+                    </li>
+                    <li>
+                        <a href="#">REGION</a>
+                    </li>
+                    <li>
+                        <a href="#">ABOUT</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <img src="imgs/menu.png" class="menu-icon">
+
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <h1>Camer</h1>
+                <p>The Sahara  is a desert on the African continent. With an area of 9,200,000 square kilometres (3,600,000 sq mi), it is the largest hot desert in the world and the third-largest desert overall, smaller only than the deserts of Antarctica and the northern Arctic.</p>
+                <button type="button">Explore</button>
+            </div>
+
+            <div class="col">
+                <div class="card card1">
+                    <h5>Western Desert</h5>
+                    <p>
+                        The sky is usually clear above the desert, and the sunshine duration is extremely high everywhere in the Sahara.
+                    </p>
+                </div>
+
+                <div class="card card2">
+                    <h5>AI Bgawan</h5>
+                    <p>
+                        The sky is usually clear above the desert, and the sunshine duration is extremely high everywhere in the Sahara.
+                    </p>
+                </div>
+
+                <div class="card card3">
+                    <h5>Pyramid of Giza</h5>
+                    <p>
+                        The sky is usually clear above the desert, and the sunshine duration is extremely high everywhere in the Sahara.
+                    </p>
+                </div>
+
+                <div class="card card4">
+                    <h5>Kalahari Desert</h5>
+                    <p>
+                        The sky is usually clear above the desert, and the sunshine duration is extremely high everywhere in the Sahara.
+                    </p>
+                </div>
+
+                
+            </div>
+
+        </div>
+
+    </div>
+</body>
+</html>
